24218a340890f555202de2561b5b91ef288a50f3
Franklin Ho <fho@twitter.com>
2014-10-20 23:26:59 -0700

    Initial Commit

---
+++ AnimationTests/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AnimationTests
+//
+//  Created by Franklin Ho on 10/20/14.
+//  Copyright (c) 2014 Franklin Ho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
